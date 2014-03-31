64d4470bd9b1d89b5b93a41eef60883f651f99ac
Ming-Zhe <law.gravitys@gmail.com>
2014-03-31 23:23:32 +0800

    create-command

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
-var createMiniHarp = require("mini-harp");
-var app = createMiniHarp();
+var createMiniHarp = require("../index");
+var parseArgs = require('minimist')(process.argv.slice(2));
 
-app.listen(4000);
+var port = parseArgs.p || parseArgs.port || 4000
+
+console.log("Starting mini-harp on http://localhost:" + port);
+
+createMiniHarp.listen(port);
--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-// var createMiniHarp = require("mini-harp")
-//   , app = createMiniHarp();
-
-// console.log("Starting mini-harp on http://localhost:4000");
-// app.listen(4000);
-
 var connect = require('connect');
 var app = connect();
 
