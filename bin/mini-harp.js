534464080743829a0184b4f49b87785e25056709
Ming-Zhe <law.gravitys@gmail.com>
2014-04-01 12:03:30 +0800

    server-static

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -6,5 +6,9 @@ var parseArgs = require('minimist')(process.argv.slice(2));
 var port = parseArgs.p || parseArgs.port || 4000
 
 console.log("Starting mini-harp on http://localhost:" + port);
-// console.log(parseArgs._);
-createMiniHarp().listen(port);
+// console.log(process.cwd());
+// console.log(parseArgs._[0]);
+
+var dir_name = parseArgs._.length > 0 ? parseArgs._[0] : process.cwd();
+
+createMiniHarp(dir_name).listen(port);
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 var connect = require('connect');
+var serveStatic = require('serve-static');
 
-module.exports = function(){
+module.exports = function(dir_name){
 	var app = connect();
-	app.use(function(req,res){
+	app.use(function(req,res,next){
 		if (req.url === '/current-time'){
 			res.end((new Date()).toISOString());
-		}
-		if (req.url === '/'){
+		}if (req.url === '/'){
 			res.end("Welcome!");
+		}else{
+			res.end("Cannot Get " + req.url);
+			next();
 		}
 	});
+	app.use(serveStatic(dir_name, {'index':'default.html'}));
 
 	return app;
 }
