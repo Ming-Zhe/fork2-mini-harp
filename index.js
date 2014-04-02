260507041f7c0d0573016f599566a3ff7990f934
Ming-Zhe <law.gravitys@gmail.com>
2014-04-02 17:59:22 +0800

    content-headers

--- index.js
+++ index.js
@@ -3,24 +3,23 @@ var serveStatic = require('serve-static');
 var makeJade = require('./lib/processor/jade');
 var makeLess = require('./lib/processor/less');
 var makeRoot = require('./lib/processor/root');
+var reject = require('./lib/processor/reject');
 
 module.exports = function(dir_name){
 	var app = connect();
 	app
+	  .use(reject())
+	  .use(serveStatic(dir_name))
 	  .use(makeRoot())
 	  .use(makeLess(dir_name))
 	  .use(makeJade(dir_name))
 	  .use(function(req,res,next){
 		if (req.url === '/current-time'){
 			res.end((new Date()).toISOString());
-		}if (req.url === '/'){
-			res.end("Welcome!");
 		}else{
 			res.end("Cannot Get " + req.url);
-			next();
 		}
 	});
-	app.use(serveStatic(dir_name));
 
 	return app;
 }
