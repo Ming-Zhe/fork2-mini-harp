8a0cbdcf8b062bdadb1d857fc3ad927fc442c236
Ming-Zhe <law.gravitys@gmail.com>
2014-04-02 14:59:51 +0800

    jade-preprocess

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
+var makeJade = require('./lib/processor/jade');
 
 module.exports = function(dir_name){
 	var app = connect();
-	app.use(function(req,res,next){
+	app
+	  .use(makeJade(dir_name))
+	  .use(function(req,res,next){
 		if (req.url === '/current-time'){
 			res.end((new Date()).toISOString());
 		}if (req.url === '/'){
